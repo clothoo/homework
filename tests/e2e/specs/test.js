5e9ca841ec8de67b9aadd6c1067d8828b28aecdd
clothoo <clothoo@hotmail.com>
2020-11-25 17:09:14 +1100

    CI test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,5 +4,6 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
     cy.contains('div', 'John Doe');
+    cy.contains('div', 'Sarah Smith');
   });
 });
